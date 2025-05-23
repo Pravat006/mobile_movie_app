bd1844792709124922975d6f42859000a460b9ba
PRAVAT BEHERA <beherapravat301@gmail.com>
2025-05-23 17:02:59 +0530

    Remove reset-project script and update TypeScript configuration to include nativewind-env.d.ts

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      className="flex items-center justify-center  text-blue-800  bg-gray-300 h-screen"
+    >
+      <Text className="text-dark-200 text-3xl ">Welcome to react-native</Text>
+    </View>
+  );
+}
