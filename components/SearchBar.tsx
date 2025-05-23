f3999823abdac459894cddec3ce66996889e509d
PRAVAT BEHERA <beherapravat301@gmail.com>
2025-05-24 01:46:37 +0530

    Add SearchBar component with basic styling

---
+++ components/SearchBar.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const SearchBar = () => {
+    return (
+        <View className='flex-row items-center justify-between bg-dark-100 rounded-full px-5 py-4'>
+            <Text className='text-white'>SearchBar</Text>
+        </View>
+    )
+}
+
+export default SearchBar
+
+// 1.05.09
