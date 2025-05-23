10076c513f8385a8729b06e2368d35ae316ed936
PRAVAT BEHERA <beherapravat301@gmail.com>
2025-05-24 01:37:57 +0530

    Add search component and new assets; define icons and images constants

---
+++ app/(tabs)/search.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const search = () => {
+    return (
+        <View className='flex-1 items-center justify-center'>
+            <Text>search</Text>
+        </View>
+    )
+}
+
+export default search
+
